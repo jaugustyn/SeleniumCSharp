d11e8a2cf3c76327f28dad28bbb9e050148ebfd7
jaugustyn <kub4a1@gmail.com>
2023-06-15 20:09:38 +0200

    Naprawiony DataDrivenTest, dodany test screenshota

--- SeleniumCSharpTutorials/SeleniumCSharpTutorial04.cs
+++ SeleniumCSharpTutorials/SeleniumCSharpTutorial04.cs
@@ -2,50 +2,61 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
-using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 
 namespace SeleniumCSharpTutorials
 {
     [TestFixture]
     class SeleniumCSharpTutorial04
     {
-
-        static IList DataDrivenTesting()
+        // Prepare test data
+        private static IEnumerable<TestCaseData> DataDrivenLoginTesting
         {
-            ArrayList list = new ArrayList();
-            list.Add("https://www.facebook.com/");
-            //list.Add("https://www.youtube.com/");
-            //list.Add("https://www.github.com/login/");
-            return list;
+            get
+            {
+                yield return new TestCaseData("https://facebook.com/", "id", "email");
+                yield return new TestCaseData("https://github.com/login/", "id", "login_field");
+                yield return new TestCaseData("https://e.wsei.edu.pl/login/index.php/", "id", "username");
+            }
         }
 
         [Test]
         [Author("Jakub", "Jakub.Augustyn@microsoft.wsei.edu.pl")]
-        [Description("Facebook login Verify")]
-        [TestCaseSource("DataDrivenTesting")]
-        public void Test1(string urlName)
+        [Description("Login field Verify")]
+        [TestCaseSource("DataDrivenLoginTesting")]
+        public void TestLoginFields(string urlName, string IdName, string IdValue)
         {
             IWebDriver driver = null;
             try
             {
+                // Enter login on various sites  
+
                 driver = new ChromeDriver();
-                driver.Manage().Window.Maximize();
-                // driver.Url = "https://www.facebook.com/";
+                //driver.Manage().Window.Maximize();
                 driver.Url = urlName;
-                IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
-                // IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='abcd']"));
-                emailTextField.SendKeys("Selenium C#");
+
+                var path = $".//*[@{IdName}='{IdValue}']";
+                IWebElement loginTextField = driver.FindElement(By.XPath(path));
+
+                loginTextField.SendKeys("Selenium C#");
                 driver.Quit();
             }
             catch (Exception e)
             {
+                // If it fails take a screen shot  
+
                 ITakesScreenshot ts = driver as ITakesScreenshot;
                 Screenshot screenshot = ts.GetScreenshot();
-                var current = Directory.GetCurrentDirectory();
-                string fullPath = Path.Combine(current, "\\Screenshots\\Screenshot1.jpeg");
+
+                var path = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().GetName().CodeBase).LocalPath);
+                string fullPath = Path.GetFullPath(Path.Combine(path, @"..\..\Screenshots\Screenshot1.jpeg"));
+
                 screenshot.SaveAsFile(fullPath, ScreenshotImageFormat.Jpeg);
+
                 Console.WriteLine(e.StackTrace);
+
                 throw;
             }
             finally
@@ -54,22 +65,58 @@ namespace SeleniumCSharpTutorials
                 {
                     driver.Quit();
                 }
+            }
+        }
+        
+
+        [Test]
+        [Description("Take screenshot test")]
+        public void TestScreenshot()
+        {
+            IWebDriver driver = null;
+            try
+            {
+                // Enter login on various sites  
+
+                driver = new ChromeDriver();
+                //driver.Manage().Window.Maximize();
+                driver.Url = "https://e.wsei.edu.pl/login/index.php/";
+
+                var path = $".//*[@idbledne='usernametez']";
+
+                IWebElement loginTextField = driver.FindElement(By.XPath(path));
+
+                loginTextField.SendKeys("Selenium C#");
+                driver.Quit();
 
             }
+            catch (Exception e)
+            {
+                // It fails and takes a screenshot  
 
+                ITakesScreenshot ts = driver as ITakesScreenshot;
+                Screenshot screenshot = ts.GetScreenshot();
+
+                var path = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().GetName().CodeBase).LocalPath);
+                var fullPath = Path.Combine(path, $@"..\..\Screenshots\Screenshot{DateTime.Now.ToString("yyyy-MM-dd HH.mm.ss")}.jpeg");
+
+                // Removing screenshots from previous tests
+                string[] filePaths = Directory.GetFiles(Path.Combine(path, $@"..\..\Screenshots\"));
+                foreach (string filePath in filePaths)
+                    File.Delete(filePath);
+
+                // Screenshots have about 100KB, but dont forget to clear the folder sometimes
+                screenshot.SaveAsFile(fullPath, ScreenshotImageFormat.Jpeg);
+                
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                if (driver != null)
+                {
+                    driver.Quit();
+                }
+            }
         }
-
-        //[Test]
-        //[Author("Bakkappa", "bakkappac@gmail.com")]
-        //[Description("Facebook login Verify")]
-        //public void Test2()
-        //{
-        //    IWebDriver driver = new ChromeDriver();
-        //    driver.Manage().Window.Maximize();
-        //    driver.Url = "https://www.facebook.com/";
-        //    IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
-        //    emailTextField.SendKeys("Selenium C#");
-        //    driver.Quit();
-        //}
     }
 }
